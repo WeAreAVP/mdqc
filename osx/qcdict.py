242ca8c414c225690b207a62d43239980ff606f3
Zaheer <zaheer@avpreserve.com>
2018-03-07 19:10:45 +0500

    MDQC-31 MacOS

--- osx/qcdict.py
+++ osx/qcdict.py
@@ -92,9 +92,12 @@ def mnfoMeta(file, useMediaInfoFile):
                             stdout=subprocess.PIPE)
         out = p.communicate()[0].splitlines()
     else:
-        import io
-        f = io.open(file, mode="r", encoding="utf-8").read()
-        out = f.splitlines()
+        try:
+            import io
+            f = io.open(file, mode="r", encoding="utf-8").read()
+            out = f.splitlines()
+        except:
+            out = []
 
     # formats the list into a dictionary
     prefix = ""
